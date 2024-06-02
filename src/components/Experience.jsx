7b3a24e304a0a906815df90f1f6f3534d389ddf4
RayanBn <bannirayan@gmail.com>
2024-06-02 20:03:16 +0200

    feat: elephant

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,31 @@
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, useAnimations, useGLTF } from "@react-three/drei";
+import { useEffect, useRef } from "react";
 
 export const Experience = () => {
-  return (
-    <>
-      <OrbitControls />
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
-    </>
-  );
+    const modelRef = useRef();
+
+    const { scene, animations } = useGLTF("/elephant.gltf");
+    const { actions, names } = useAnimations(animations, modelRef);
+
+    useEffect(() => {
+        actions[names[0]].reset().fadeIn(0.5).play();
+        return () => {
+            actions[names[0]].reset().fadeOut(0.5).stop();
+        };
+    }, [actions]);
+
+    return (
+        <>
+            <OrbitControls />
+
+            <ambientLight intensity={0.5} />
+            <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+
+            <primitive
+                ref={modelRef}
+                scale={[0.3, 0.3, 0.3]}
+                object={scene}
+            />
+        </>
+    );
 };
